547cd34000e8db70879fb5e9a1fa2caab855a6dd
david-gimeno <dagigo1@dsic.upv.es>
2024-11-24 17:23:52 +0000

    Refactor code

--- scripts/runs/experiments/self_ssl/gita.sh
+++ scripts/runs/experiments/self_ssl/gita.sh
@@ -8,7 +8,7 @@ for seed in 12 21 33 42 52; do
 for f in 0 1 2 3 4; do
 
   CUDA_VISIBLE_DEVICES=0 python scripts/model_pipeline/pipeline.py \
-      --config ./configs/attention.yaml \
+      --config ./configs/framework.yaml \
       --training-dataset ./splits/$dataset/fold_$f/fulltrain.csv \
       --validation-dataset ./splits/$dataset/fold_$f/test.csv \
       --test-dataset ./splits/$dataset/fold_$f/test.csv \
